626e82ef0844eeba951f5f59c672a8fcbe511717
Matej Lukac <matej.lukac@proup.sk>
2021-08-17 22:00:30 +0200

    desktop version

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import $ from "jquery";
+window.jQuery = $;
+window.$ = $;
+
+//IMPORT SCSS
+import "./styles/main.scss";
+
+//IMPORT SCRIPTS
+import "./scripts/slick";
+import "./scripts/burger-menu";
